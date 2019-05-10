2f6e08498a224fd285df1c0aa4cbf731dc3d9e0e
Andrej Adamcik <adamcikado@gmail.com>
2019-05-10 14:45:49 +0200

    update export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import Lango from './lango';
+import Lango from 'lango';
 import {install} from './install';
 
-export {Lango, install};
+export default class LangoVue extends Lango {
+	subscribeData(vm) {
+		vm._i18nListener = vm._i18nListener || (() => {
+			vm.$nextTick(function() {
+				this.$forceUpdate();
+			});
+		});
+
+		this.on('update', vm._i18nListener);
+	}
+	
+	unsubscribeData(vm) {
+		this.off('update', vm._i18nListener);
+	}
+};
+
+LangoVue.install = install;
